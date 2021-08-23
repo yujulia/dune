7a693550a1aaae6d165f589aa5907e829cc72490
Julia Yu <juliayu@XIII.local>
2021-08-22 21:13:31 -0700

    add assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,90 @@
 /** -------------------------------------------------------- 
 */
-
 const { series, parallel, src, dest, watch } = require('gulp');
-
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
+const log = require('gulplog');
+
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const babelify = require('babelify');
+const uglify = require('gulp-uglify');
+
 const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat')
-const merge = require('merge-stream')
 
 
 /** -------------------------------------------------------- 
+ * Concat css and sass streams after compiling sass
 */
 
 function styles() {
-	console.log("SASSing...")
+	console.log("SASSing...");
 
-	var normStream = src('node_modules/normalize.css/normalize.css')
+	var normStream = src('node_modules/normalize.css/normalize.css');
 	var sassStream = src('sass/**/*.{scss,sass}')
-		.pipe(sass().on('error', sass.logError))
+		.pipe(sass().on('error', sass.logError));
 
 	return merge(normStream, sassStream)
 		.pipe(concat('main.css'))
-		.pipe(dest('css/'))
-}
-
-function watcher() {
-	console.log("Watching...")
-	watch(['sass/**/*.{scss,sass}'], styles)
+		.pipe(dest('deploy/'));
 }
 
 /** -------------------------------------------------------- 
 */
 
-// function test() {
-// 	console.log("test...")
-// 	return src('js/*.js')
-// 	    .pipe(babel())
-// 	    .pipe(uglify())
-// 	    .pipe(rename({ extname: '.min.js' }))
-// 	    .pipe(dest('js/'));
-// }
+function scripts() {
+	var options = {
+		entries: ['js/index.js'],
+  		debug: true,
+  		transform: ['babelify'],
+	};
+
+	console.log("browserify...")
+	return browserify(options)
+		.bundle()
+		.on('error', log.error.bind(log, 'Browserify Error'))
+		.pipe(source('bundle.js'))
+		.pipe(dest('deploy/'));
+}
+
+
+
+function old() {
+	console.log("Scripts...");
+	// var rellaxStream = src('node_modules/rellax/rellax.js')
+
+	// return browserify(options)
+	// 	.bundle()
+	// 	.on('error', log.error.bind(log, 'Browserify Error'))
+	// 	.pipe(source('browindex.js'))
+	// 	.pipe(merge(rellaxStream))
+	// 	.pipe(concat('bundle.js'))
+	// 	.pipe(dest('deploy/'));
+
+	// var jsStream = src('js/**/*.js')
+	// var scrollStream = src('node_modules/rellax/rellax.js')
+	// // return merge(locoStream, jsStream)
+
+	// return merge(scrollStream, jsStream)
+	// 	.pipe(concat('bundle.js'))
+	//     // .pipe(babel())
+	//     // .pipe(uglify())
+	//     .pipe(dest('deploy/'));
+}
+
+/** -------------------------------------------------------- 
+ * who watches the watcher
+*/
+
+function watcher() {
+	console.log("Watching...");
+	parallel(scripts, styles);
+	watch(['sass/**/*.{scss,sass}'], { ignoreInitial: false }, styles);
+	watch(['js/**/*.js'], { ignoreInitial: false }, scripts);
+}
 
 /** -------------------------------------------------------- public tasks
 */
 
-
-exports.default = watcher
+exports.default = watcher;
---
+++ js/index.js
@@ -0,0 +1,8 @@
+function onLoad() {
+	var rellax = new Rellax('.rellax', {
+    center: true
+  });	
+	console.log("index js loaded")
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
