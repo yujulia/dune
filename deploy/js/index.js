dbab9b1f7891de644fce2f48592e5ba2940e11c0
Julia Yu <juliayu@XIII.local>
2021-08-25 02:05:30 -0700

    add

--- deploy/js/index.js
+++ deploy/js/index.js
@@ -951,13 +951,22 @@
 
 	const imagePath = "deploy/img/";
 	let skySource = imagePath + "sky.png";
-	let singles = ['baron-dark', 'baron', 'bubble-bottom', 'bubble-top', 'goldenpath', 'moon'].map(function (e) {
+	let singles = ['baron-dark', 'baron', 'bubble-bottom', 'bubble-top', 'goldenpath', 'moon', 'title', 'title-black'].map(function (e) {
 	  return imagePath + e + ".png";
 	});
 	let pairs = ['arrakis', 'dune', 'fedaykin', 'fireplace', 'gomjabbar', 'keep', 'muaddib', 'sardaukar', 'shaihulud', 'twomoons', 'water', 'witch'].flatMap(e => [imagePath + "fore-" + e + ".png", imagePath + "back-" + e + ".png"]);
 	let rooms = Array.from({
 	  length: 8
-	}, (_, index) => imagePath + "room-" + index + ".png"); // ---------------------------------- start parallax plugin
+	}, (_, index) => imagePath + "room-" + index + ".png");
+	let roomsMobile1 = [1, 2, 3, 4, 5, 6, 7].map(function (e) {
+	  return imagePath + "room-" + e + "-0.png";
+	});
+	let roomsMobile2 = [1, 2, 3, 4, 7].map(function (e) {
+	  return imagePath + "room-" + e + "-1.png";
+	});
+	let roomsMobile3 = ["fore-arrakis-0", "fore-arrakis-1", "back-arrakis-0", "back-arrakis-1"].map(function (e) {
+	  return imagePath + e + ".png";
+	}); // ---------------------------------- start parallax plugin
 
 	function initializeParallax() {
 	  var rellax$1 = new rellax('.rellax', {
@@ -968,7 +977,8 @@
 
 
 	function preload() {
-	  var images = [].concat(singles, pairs, rooms);
+	  var images = [].concat(singles, pairs, rooms, roomsMobile1, roomsMobile2, roomsMobile3);
+	  console.log("images", images);
 	  prefetchImages(images).then(() => {
 	    document.body.classList.add("loaded");
 	    document.getElementById("preloader").remove();
--- js/index.js
+++ js/index.js
@@ -13,6 +13,8 @@ let singles = [
   'bubble-top',
   'goldenpath',
   'moon',
+  'title',
+  'title-black'
 ].map(function(e) {
 	return imagePath + e + ".png";
 });
@@ -34,6 +36,23 @@ let pairs = [
 
 let rooms = Array.from({length: 8}, (_, index) => imagePath + "room-" + index + ".png");
 
+let roomsMobile1 = [1,2,3,4,5,6,7].map(function(e) { 
+	return imagePath + "room-" + e + "-0.png";
+});
+
+
+let roomsMobile2 = [1,2,3,4,7].map(function(e) { 
+	return imagePath + "room-" + e + "-1.png";
+});
+
+let roomsMobile3 = [
+	"fore-arrakis-0",
+	"fore-arrakis-1",
+	"back-arrakis-0",
+	"back-arrakis-1",
+].map(function(e) { 
+	return imagePath + e + ".png";
+});
 
 // ---------------------------------- start parallax plugin
 
@@ -47,7 +66,9 @@ function initializeParallax() {
 // ---------------------------------- preload 
 
 function preload() {
-	var images = [].concat(singles, pairs, rooms)
+	var images = [].concat(singles, pairs, rooms, roomsMobile1, roomsMobile2, roomsMobile3);
+
+	console.log("images", images);
 
 	prefetchImages(images)
 	  .then(() => {
